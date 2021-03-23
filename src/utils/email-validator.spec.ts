61cb32c7565d6695362026e1c2a9fa1d5e21271d
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-03-23 20:46:45 -0300

    feat: create EmailValidator

---
+++ src/utils/email-validator.spec.ts
@@ -0,0 +1,41 @@
+import validator from 'validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
+
+jest.mock('validator', () => {
+  return {
+    isEmail (): boolean {
+      return true
+    }
+  }
+})
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
+describe('Email Validator Adapter', () => {
+  test('should return false if validator returns false', async () => {
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+
+    const isValid = sut.isValid('invalid_email@email.com')
+
+    expect(isValid).toBe(false)
+  })
+
+  test('should return true if validator returns true', async () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_email@email.com')
+
+    expect(isValid).toBe(true)
+  })
+
+  test('should call validator with correct email', async () => {
+    const sut = makeSut()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+
+    sut.isValid('email@email.com')
+
+    expect(isEmailSpy).toHaveBeenCalledWith('email@email.com')
+  })
+})
