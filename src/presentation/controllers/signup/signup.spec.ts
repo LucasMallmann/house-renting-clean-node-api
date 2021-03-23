112c7d49827df3daa23248a7c2db2270951a6e80
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-03-22 22:16:54 -0300

    refactor: move sut creation to a helper method

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,8 +1,34 @@
 import { SignUpController } from './signup'
 
+interface SutTypes {
+  sut: SignUpController
+}
+
+const makeSut = (): SutTypes => {
+  return {
+    sut: new SignUpController()
+  }
+}
+
 describe('SignUp Controller', () => {
   test('should return 400 if no name is provided', async () => {
-    const sut = new SignUpController()
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  test('should return 400 if no emai. is provided', async () => {
+    const { sut } = makeSut()
 
     const httpRequest = {
       body: {
