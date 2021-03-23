c53c2f4108953f8c36b7c3cf6e5210f14f0a4deb
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-03-23 20:00:08 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,6 +1,6 @@
 import { IController, IHttpRequest, IHttpResponse } from './signup-protocols'
 import { IEmailValidator } from '../../protocols/email-validator'
-import { badRequest } from '../../helpers/http-helper'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { InvalidParamError, MissingParamError } from '../../errors'
 
 export class SignUpController implements IController {
@@ -13,22 +13,26 @@ export class SignUpController implements IController {
   }
 
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
-    const { email } = httpRequest.body
+      const { email } = httpRequest.body
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
+
+      const isEmailValid = this.emailValidator.isValid(email)
+
+      if (!isEmailValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      return new Promise(resolve => resolve(null as IHttpResponse))
+    } catch (error) {
+      return serverError(error)
     }
-
-    const isEmailValid = this.emailValidator.isValid(email)
-
-    if (!isEmailValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
-    return new Promise(resolve => resolve(null as IHttpResponse))
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,12 @@
+import { ServerError } from '../errors/server-error'
 import { IHttpResponse } from '../protocols'
 
 export const badRequest = (error: Error): IHttpResponse => ({
   statusCode: 400,
   body: error
+})
+
+export const serverError = (error: Error): IHttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack)
 })
