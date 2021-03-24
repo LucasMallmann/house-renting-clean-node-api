35a326410de699193f77ac95243a8e2c315370d6
Lucas Mallmann <lucasmallmann76@gmail.com>
2021-03-23 22:10:59 -0300

    test: ensure AccounMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,37 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
 
 describe('Account Mongo Repository', () => {
-  test('whatever', () => {})
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
+  })
+
+  test('should return an account on success', async () => {
+    const sut = makeSut()
+
+    const account = await sut.add({
+      email: 'email@email.com',
+      name: 'any_name',
+      password: 'any_password'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('email@email.com')
+    expect(account.password).toBe('any_password')
+  })
 })
